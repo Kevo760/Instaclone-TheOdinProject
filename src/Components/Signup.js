6a12e5a35477e38061e2be97ba5d7b35fb00afa7
kevin <73809530+TheLegend760@users.noreply.github.com>
2023-04-27 23:03:16 -0700

    Added firebase and added more functions on signup

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from "styled-components"
 import { LogoText } from '../Styled Components/TextStyles'
+import {BiImageAdd} from 'react-icons/bi'
+import { CircleProfileLarge } from '../Styled Components/CircleProfileImg'
 
 
 
@@ -11,15 +13,32 @@ const SignUpSection = styled.div`
   align-items: center;
   text-align: center;
   min-height: 100vh;
+  
 `
 
-
-const SignUpBox = styled.form`
+const SignUpForm = styled.form`
     width: 350px;
     height: 400px;
     padding: 16px;
     border: 1px solid rgba(229, 229, 229);
-    height: 450px;
+    height: fit-content;
+    .add-profile-pic {
+        margin-top: 16px;
+    }
+    .add-profile-pic label {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 10px;
+        cursor: pointer;
+    }
+    .add-img-icon {
+        scale: 1.8;
+    }
+    .error-text {
+        color: tomato;
+        font-weight: bold;
+    }
 `
 
 const SmallSignBox = styled.div`
@@ -51,7 +70,7 @@ const SignInput = styled.input`
 `
 
 const SignButton = styled.button`
-    margin-top: 20px;
+    margin: 20px;
     width: 80%;
     padding: 10px;
     color: white;
@@ -68,32 +87,85 @@ const SignButton = styled.button`
     }
 `
 
+
 function Signup() {
+  const [profileImage, setProfileImage] = useState(null)
+  const [showError, setShowError] = useState(false)
+
+  const showProfileImg = <div><CircleProfileLarge src={profileImage} alt='profile image'/></div>
+
+  const handleCreateAccount = (e) => {
+    e.preventDefault()
+    const email = e.target[0].value
+    const fullName = e.target[1].value
+    const userName = e.target[2].value
+    const password = e.target[3].value
+    const file = e.target[4].files[0]
+    console.log(password)
+  }
+
+  const onImageChange = (e) => {
+    if(e.target.files && e.target.files[0]) {
+        setProfileImage(URL.createObjectURL(e.target.files[0]))
+    }
+  }
+
   return (
     <SignUpSection>
-        <SignUpBox>
+        <SignUpForm onSubmit={handleCreateAccount}>
             <LogoText>Instaclone</LogoText>
+            {
+                profileImage ?
+                showProfileImg
+                :
+                null
+            }
 
             <SignInput 
                 placeholder='Email'
                 type='email'
+                required
             />
             <SignInput 
                 placeholder='Full Name'
                 type='text'
+                required
             />
             <SignInput 
                 placeholder='Username'
                 type='text'
+                required
             />
             <SignInput 
                 placeholder='Password'
                 type='password'
+                required
             />
+            <div className='add-profile-pic'>
+                <input 
+                    style={{display: 'none'}}
+                    type='file'
+                    id='file'
+                    accept="image/*"
+                    onChange={onImageChange}
+                    required
+                />
+                <label htmlFor='file'>
+                    <BiImageAdd className='add-img-icon'/>
+                    <span>Add profile picture</span>
+                </label>
+            </div>
+            
+            <SignButton type='submit'>Sign up</SignButton>
 
-            <SignButton>Sign up</SignButton>
+            {
+                showError ?
+                <span className='error-text'>Something went wrong, try again.</span>
+                :
+                null
+            }
 
-        </SignUpBox>
+        </SignUpForm>
 
         <SmallSignBox>Have an account? <a>Log in</a></SmallSignBox>
     </SignUpSection>
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6r_4w_84T1ndbpAgbpBfnAGh7mM49y7A",
+  authDomain: "instaclone-67a93.firebaseapp.com",
+  projectId: "instaclone-67a93",
+  storageBucket: "instaclone-67a93.appspot.com",
+  messagingSenderId: "498726185474",
+  appId: "1:498726185474:web:a2ca8e933cd9bbf9fb951b",
+  measurementId: "G-5ETPPNB21W"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
